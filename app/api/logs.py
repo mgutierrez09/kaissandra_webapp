a4c3365ab94a4a100d897c895b3e24a569e27ee8
mgutierrez09 <ma.gutierrez.estevez@gmail.com>
2019-06-24 18:26:12 +0200

    logs entry point added

---
+++ app/api/logs.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 17:12:10 2019
+
+@author: mgutierrez
+"""
+from flask import request
+from app.api import bp
+from app.api.auth import token_auth
+from app.api.errors import bad_request
+import datetime as dt
+
+@bp.route('/logs/traders/', methods=['POST'])
+@token_auth.login_required
+def get_trader_log():
+    """  """
+    json_data = request.get_json() or {}
+    if 'Message' not in json_data:
+        return bad_request("Message not included in json.")
+    if 'Name' not in json_data:
+        return bad_request("Name of trader not included in json.")
+    print(dt.datetime.strftime(dt.datetime.utcnow,'%d.%m.%y %H:%M:%S')+' trader '+json_data['Name']+': '+json_data['Message'])
+    
+@bp.route('/logs/networks', methods=['POST'])
+@token_auth.login_required
+def get_network_log():
+    """  """
+    json_data = request.get_json() or {}
+    if 'Message' not in json_data:
+        return bad_request("Message not included in json.")
+    print(dt.datetime.strftime(dt.datetime.utcnow,'%d.%m.%y %H:%M:%S')+' network: '+json_data['Message'])
