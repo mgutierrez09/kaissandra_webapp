31677302b97a8705d1c54cb8dbe26d8c53474838
mgutierrez09 <ma.gutierrez.estevez@gmail.com>
2020-01-26 15:18:55 +0100

    debugging heroku server

--- app/api/traders.py
+++ app/api/traders.py
@@ -452,6 +452,7 @@ def open_session(id):
     result = session_sch.dump(session)
     # update params structure
     params[session.id] = {}
+    print(params)
     return jsonify({
         'message': mess,
         'Session': result,
@@ -522,13 +523,17 @@ def params_enquired(id):
         return bad_request("Session does not exist")
     if not session.running:
         return bad_request("Session not running")
-    # TODO: Find strategies with new parameters and send them back
+    if id not in params:
+        json_return = jsonify({
+        'message': "Error. "+str(id)+" not in params",
+        'params': {},
+        })
     json_return = jsonify({
         'message': "New params",
-        'params': params[str(id)],
+        'params': params[id],
     })
-    for key in list(params[str(id)]):
-        del params[str(id)][key]
+    for key in list(params[id]):
+        del params[id][key]
     #print(params)
     return json_return
 
